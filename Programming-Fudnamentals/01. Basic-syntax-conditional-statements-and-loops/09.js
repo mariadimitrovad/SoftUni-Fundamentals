0b9a97a414640b4598a39e5725e2066b08267e10
Maria Dimitrova <piinkygiirl@abv.bg>
2023-10-02 19:10:53 +0300

    new

--- Programming-Fudnamentals/01. Basic-syntax-conditional-statements-and-loops/09.js
+++ Programming-Fudnamentals/01. Basic-syntax-conditional-statements-and-loops/09.js
@@ -1,29 +1,7 @@
-function solve(input){
-let username = input[0];
-let pass = '';
-let checkedPass = 0;
-
-for(let i = username.length - 1; i >= 0; i--){
-  pass += username[i];
+function solve(num){
+while (num >= 1){
+  console.log(n);
+  num--;
 }
-
-let guess = input[1];
-let index = 2;
-
-while(guess != pass){
-  checkedPass++;
-  
-  if(checkedPass == 4){
-    console.log(`User ${username} blocked!`);
-    return;
-  } 
-
-  console.log(`Incorrect password. Try again.`);
-
-  guess = input[index];
-  index++;
 }
-
-console.log(`User ${username} logged in.`);
-}
-solve(['Acer','login','go','let me in','recA']);
+solve(5);
--- Programming-Fudnamentals/11.js
+++
@@ -1,6 +0,0 @@
-function solve(num){
-let totalSum = 0;
-
-for(let i = n;)
-}
-solve(5);
